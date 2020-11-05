d5800d4911f440a095a1eac231e19399d5a3ab6e
Sherlock Liu <530317349@qq.com>
2020-11-06 00:08:02 +0800

    init project

---
+++ src/main/java/com/whsa/sherlock/seaweedfsmanagement/SeaweedfsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.whsa.sherlock.seaweedfsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaweedfsManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeaweedfsManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whsa/sherlock/seaweedfsmanagement/SeaweedfsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whsa.sherlock.seaweedfsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaweedfsManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
